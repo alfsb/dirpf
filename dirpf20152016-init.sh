fdda1b57fed4f45d4c943168719fab9b4127ef00
alfsb <alfsb@users.noreply.github.com>
2016-03-04 00:06:34 -0300

    Update dirpf20152016-init.sh

--- dirpf20152016-init.sh
+++ dirpf20152016-init.sh
@@ -3,7 +3,7 @@ if [ -e "/usr/lib/jvm/java-7-oracle/jre/bin/java" ]
 then
 	echo "Rodando com o Java 7 (Oracle)"
 	/usr/lib/jvm/java-7-oracle/jre/bin/java -Xms128M -Xmx512M -jar /home/andre/ProgramasRFB/IRPF2016/irpf.jar
-	return
+	exit
 fi
 echo "No Java 7 (Oracle)";
 
@@ -11,6 +11,6 @@ if [ -e "/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java" ]
 then
 	echo "Rodando com o Java 7 (OpenJDK)";
 	/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java -Xms128M -Xmx512M -jar /home/andre/ProgramasRFB/IRPF2016/irpf.jar
-	return
+	exit
 fi
 echo "No Java 7 (OpenJDK)";
