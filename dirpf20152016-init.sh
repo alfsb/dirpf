797b35072093c9ffccb740471210cc43adba156d
alfsb <alfsb@users.noreply.github.com>
2016-03-09 10:47:26 -0300

    Update dirpf20152016-init.sh

--- dirpf20152016-init.sh
+++ dirpf20152016-init.sh
@@ -1,4 +1,6 @@
 
+# Ver: http://www.bacci.com.br/andre/blog/2016/02/erro-dirpf-for-input-string-9-e/
+
 if [ -e "/usr/lib/jvm/java-7-oracle/jre/bin/java" ]
 then
 	echo "Rodando com o Java 7 (Oracle)"
